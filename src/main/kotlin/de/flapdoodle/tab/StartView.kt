2542579e659c43e3f10af0e14a9d9dde48990c6e
Michael Mosmann <michael@mosmann.de>
2020-01-30 16:04:42 +0100

    starter

--- src/main/kotlin/de/flapdoodle/tab/StartView.kt
+++ src/main/kotlin/de/flapdoodle/tab/StartView.kt
@@ -1,11 +1,17 @@
 package de.flapdoodle.tab
 
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
+import javafx.scene.paint.Color
+import javafx.scene.paint.Paint
 import tornadofx.*
 
 class StartView : View("My View") {
   override val root = borderpane {
     center = label {
       text="Tab"
+
+      background = Background(BackgroundFill(Color.RED,null,null))
     }
   }
 }
