3a989ed25c5246ccc82edc1d0c7b7464b22cea92
Michael Mosmann <michael@mosmann.de>
2020-02-12 08:00:28 +0100

    custom nodes

--- src/main/kotlin/de/flapdoodle/tab/graph/events3/GraphNode.kt
+++ src/main/kotlin/de/flapdoodle/tab/graph/events3/GraphNode.kt
@@ -1,15 +1,22 @@
 package de.flapdoodle.tab.graph.events3
 
 import javafx.event.EventHandler
+import javafx.event.EventTarget
+import javafx.scene.Group
+import javafx.scene.Node
 import javafx.scene.input.MouseEvent
 import javafx.scene.paint.Color
 import tornadofx.*
+
+fun <T: UIComponent> EventTarget.uiGroup(parentComponent: T, initialChildren: Iterable<Node>? = null, op: GraphNode.UIComponentGroup<T>.() -> Unit = {}) =
+    opcr<GraphNode.UIComponentGroup<T>>(this, GraphNode.UIComponentGroup(parentComponent).apply { if (initialChildren != null) children.addAll(initialChildren) }, op)
+
 
 class GraphNode(
     private val x: Number = 0.0,
     private val y: Number = 0.0
 ) : Fragment() {
-  override val root = group {
+  override val root = uiGroup(this) {
     rectangle(x = x, y = y) {
       style {
         fill = Color.BLUE
@@ -25,4 +32,11 @@ class GraphNode(
   }
 
   data class EnterNodeEvent(val parent: GraphNode) : FXEvent()
+
+  class UIComponentGroup<T: UIComponent>(
+      private val parentComponent: T
+  ) : Group() {
+
+    fun parentComponent() = parentComponent
+  }
 }
