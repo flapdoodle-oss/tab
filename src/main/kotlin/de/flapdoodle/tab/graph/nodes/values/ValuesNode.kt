6354961963e78384384e53fcf5446ed88aa54f3f
Michael Mosmann <michael@mosmann.de>
2020-02-18 19:09:49 +0100

    change to constructor

--- src/main/kotlin/de/flapdoodle/tab/graph/nodes/values/ValuesNode.kt
+++ src/main/kotlin/de/flapdoodle/tab/graph/nodes/values/ValuesNode.kt
@@ -3,16 +3,28 @@ package de.flapdoodle.tab.graph.nodes.values
 import de.flapdoodle.tab.graph.nodes.AbstractGraphNode
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.scene.Group
 import javafx.scene.Node
+import javafx.scene.control.TableView
 import tornadofx.*
 
 class ValuesNode : AbstractGraphNode() {
 
-  private val list = listOf(
-      Dummy("Klaus", 17),
-      Dummy(null, 19),
-      Dummy(null, null)
-  ).toObservable()
+  override fun content() = table()
+
+  fun table(): TableView<Dummy> {
+    val list = listOf(
+        Dummy("Klaus", 17),
+        Dummy(null, 19),
+        Dummy(null, null)
+    ).toObservable()
+
+    return tableview(list) {
+      isEditable = true
+      column("name", Dummy::nameProperty).makeEditable()
+      column("age", Dummy::ageProperty).makeEditable()
+    }
+  }
 
   class Dummy(private val name: String?, private val age: Int?) {
     val nameProperty = SimpleStringProperty(name)
@@ -20,14 +32,6 @@ class ValuesNode : AbstractGraphNode() {
       if (age != null) {
         set(age)
       }
-    }
-  }
-
-  override fun content(): Node {
-    return tableview(list) {
-      isEditable = true
-      column("name", Dummy::nameProperty).makeEditable()
-      column("age", Dummy::ageProperty).makeEditable()
     }
   }
 }
