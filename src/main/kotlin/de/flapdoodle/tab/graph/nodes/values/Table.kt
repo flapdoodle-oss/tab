fb86b5744c93771dfc04e60d7f55aeacbac5b894
Michael Mosmann <michael@mosmann.de>
2020-02-18 22:58:35 +0100

    column width fun

--- src/main/kotlin/de/flapdoodle/tab/graph/nodes/values/Table.kt
+++ src/main/kotlin/de/flapdoodle/tab/graph/nodes/values/Table.kt
@@ -3,29 +3,34 @@ package de.flapdoodle.tab.graph.nodes.values
 import de.flapdoodle.tab.graph.nodes.NodeFactory
 import javafx.event.EventTarget
 import javafx.scene.Parent
+import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
 import tornadofx.*
 
-class Table : Fragment(), () -> TableView<Dummy> {
+class Table : () -> VBox {
   val list = listOf(
       Dummy("Klaus", 17),
       Dummy(null, 19),
       Dummy(null, null)
   ).toObservable()
 
-  override val root = tableview(list) {
-    isEditable = true
-    column("name", Dummy::nameProperty).makeEditable()
-    column("age", Dummy::ageProperty).makeEditable()
-  }
+  override fun invoke(): VBox {
+    return VBox().apply {
+      val table = tableview(list) {
+        isEditable = true
+        vgrow = Priority.ALWAYS
+        column("name", Dummy::nameProperty).makeEditable()
+        column("age", Dummy::ageProperty).makeEditable()
+      }
 
-  override fun invoke(): TableView<Dummy> {
-    return TableView<Dummy>().apply {
-      this.items = list
 
-      isEditable = true
-      column("name", Dummy::nameProperty).makeEditable()
-      column("age", Dummy::ageProperty).makeEditable()
+      hbox {
+        textfield("foo") {
+          prefWidthProperty().bind(table.columns.get(0).widthProperty())
+        }
+      }
     }
   }
 }
--- src/main/kotlin/de/flapdoodle/tab/graph/nodes/values/ValuesNode.kt
+++ src/main/kotlin/de/flapdoodle/tab/graph/nodes/values/ValuesNode.kt
@@ -2,5 +2,6 @@ package de.flapdoodle.tab.graph.nodes.values
 
 import de.flapdoodle.tab.graph.nodes.AbstractGraphNode
 import javafx.scene.control.TableView
+import javafx.scene.layout.VBox
 
-class ValuesNode : AbstractGraphNode<TableView<Dummy>>(Table())
+class ValuesNode : AbstractGraphNode<VBox>(Table())
