d548a24ff92a30b52a0b796b1fe65cf423ad2aff
Michael Mosmann <michael@mosmann.de>
2021-08-22 09:06:03 +0200

    cleanup

--- src/main/kotlin/de/flapdoodle/fx/starter/NodeEditorView.kt
+++ src/main/kotlin/de/flapdoodle/fx/starter/NodeEditorView.kt
@@ -1,8 +1,6 @@
 package de.flapdoodle.fx.starter
 
-import de.flapdoodle.fx.graph.AutoScrollingWindow
 import de.flapdoodle.fx.graph.GraphView
-import de.flapdoodle.fx.graph.PanningWindow
 import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
 import tornadofx.View
@@ -12,15 +10,8 @@ import tornadofx.vbox
 class NodeEditorView : View("Node Editor") {
     override val root = vbox {
         val graphView = GraphView()
-        val panningWindow = object : AutoScrollingWindow() {
-            init {
-                setAutoScrollingEnabled(true)
-                setContent(graphView)
-            }
-        }
 
-
-        children+= panningWindow
+        children+= graphView
         button("click me") {
 
         }
