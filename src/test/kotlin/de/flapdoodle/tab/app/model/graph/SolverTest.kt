6043a8fb268008941c3c94ea96e433577431f04c
Michael Mosmann <michael@mosmann.de>
2024-02-24 10:15:57 +0100

    test solver

---
+++ src/test/kotlin/de/flapdoodle/tab/app/model/graph/SolverTest.kt
@@ -0,0 +1,24 @@
+package de.flapdoodle.tab.app.model.graph
+
+import de.flapdoodle.tab.app.model.Node
+import de.flapdoodle.tab.app.model.Tab2Model
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolverTest {
+    @Test
+    fun emptyModelDoesNotTriggerAnything() {
+        val result = Solver.solve(Tab2Model())
+        assertThat(result.nodes).isEmpty()
+    }
+
+    @Test
+    fun singleNodeWithNoConnections() {
+        val source = Tab2Model(listOf(Node.Constants("const")))
+        val changed = Solver.solve(source)
+
+        assertThat(changed).isEqualTo(source)
+    }
+}
