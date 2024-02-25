ee8c3d27cfb0bec2bfc130c2cff6ebb5f5d00bc6
Michael Mosmann <michael@mosmann.de>
2024-02-25 08:13:06 +0100

    solver

--- src/main/kotlin/de/flapdoodle/tab/app/model/Tab2Model.kt
+++ src/main/kotlin/de/flapdoodle/tab/app/model/Tab2Model.kt
@@ -1,7 +1,14 @@
 package de.flapdoodle.tab.app.model
 
+import de.flapdoodle.kfx.types.Id
+
 data class Tab2Model(
     val nodes: List<Node> = emptyList()
 ) {
-    
+
+    fun node(id: Id<out Node>): Node {
+        val matching = nodes.filter { it.id == id }
+        require(matching.size == 1) {"more or less then one node with $id found: $matching"}
+        return  matching[0]
+    }
 }
--- src/main/kotlin/de/flapdoodle/tab/app/model/graph/Solver.kt
+++ src/main/kotlin/de/flapdoodle/tab/app/model/graph/Solver.kt
@@ -2,9 +2,11 @@ package de.flapdoodle.tab.app.model.graph
 
 import de.flapdoodle.graph.GraphAsDot
 import de.flapdoodle.graph.Graphs
+import de.flapdoodle.graph.VerticesAndEdges
 import de.flapdoodle.tab.app.model.Node
 import de.flapdoodle.tab.app.model.Tab2Model
 import de.flapdoodle.tab.app.model.calculations.Calculation
+import de.flapdoodle.tab.app.model.calculations.InputSlot
 import de.flapdoodle.tab.app.model.connections.Source
 import de.flapdoodle.tab.app.model.data.ColumnId
 import de.flapdoodle.tab.app.model.data.SingleValueId
@@ -13,6 +15,70 @@ import org.jgrapht.graph.DefaultEdge
 object Solver {
 
     fun solve(model: Tab2Model): Tab2Model {
+        val roots = verticesAndEdges(model)
+        var updated = model
+        roots.forEach { ve ->
+            updated = update(updated, ve)
+        }
+        return updated
+    }
+
+    private fun update(model: Tab2Model, ve: VerticesAndEdges<Vertex, DefaultEdge>): Tab2Model {
+        var updated = model
+        if (ve.loops().isEmpty()) {
+            ve.vertices().forEach { updated = update(updated, it) }
+        } else {
+            throw IllegalArgumentException("loops: ${ve.loops()}")
+        }
+        return updated
+    }
+
+    private fun update(model: Tab2Model, vertex: Vertex): Tab2Model {
+        var updated = model
+        val node = model.node(vertex.node)
+        when (node) {
+            is Node.Calculated<*> -> {
+                val matching = when (vertex) {
+                    is Vertex.Column<*> -> {
+                        node.calculations.list.filter { c ->
+                            c is Calculation.Tabular<*,*> && c.destination == vertex.columnId
+                        }
+                    }
+                    is Vertex.SingleValue -> {
+                        node.calculations.list.filter { c ->
+                            c is Calculation.Aggregation && c.destination == vertex.valueId
+                        }
+                    }
+                }
+                require(matching.size==1) {"more or less then one matching calculation for $vertex: $matching"}
+                val calculation = matching.first()
+
+                updated = update(updated, node, calculation)
+            }
+            else -> {
+                println("can skip $node")
+            }
+        }
+        return updated
+    }
+
+    private fun <K: Comparable<K>> update(model: Tab2Model, node: Node.Calculated<K>, calculation: Calculation): Tab2Model {
+        val sourceVariables = calculation.formula.variables()
+        val neededInputs = node.calculations.inputs.filter { it.mapTo.intersect(sourceVariables).isNotEmpty() }
+        val missingSources = neededInputs.filter { it.source==null }
+        if (missingSources.isEmpty()) {
+            val sources = neededInputs.associateBy { it.source!! }
+            
+        } else {
+            println("missing sources: $missingSources")
+        }
+
+        return model
+    }
+
+    private fun verticesAndEdges(
+        model: Tab2Model
+    ): Collection<VerticesAndEdges<Vertex, DefaultEdge>> {
         val graph = Graphs.with(Graphs.graphBuilder(Graphs.directedGraph(Vertex::class.java, DefaultEdge::class.java)))
             .build { builder ->
                 model.nodes.forEach { node ->
@@ -22,23 +88,26 @@ object Solver {
                                 builder.addVertex(Vertex.SingleValue(node.id, value.id))
                             }
                         }
+
                         is Node.Table<*> -> {
                             node.columns.forEach { column ->
                                 builder.addVertex(Vertex.Column(node.id, column.id))
                             }
                         }
+
                         is Node.Calculated<*> -> {
                             node.calculations.forEach { calculation ->
                                 when (calculation) {
-                                    is Calculation.Tabular<*,*> -> {
+                                    is Calculation.Tabular<*, *> -> {
                                         builder.addVertex(Vertex.Column(node.id, calculation.destination))
                                     }
+
                                     is Calculation.Aggregation -> {
                                         builder.addVertex(Vertex.SingleValue(node.id, calculation.destination))
                                     }
                                 }
                             }
-                            node.calculations.inputs.forEach {input ->
+                            node.calculations.inputs.forEach { input ->
                                 when (input.source) {
                                     is Source.ColumnSource<*> -> {
                                         val sourceVertex = Vertex.Column(input.source.node, input.source.columnId)
@@ -65,6 +134,7 @@ object Solver {
                                             builder.addEdge(sourceVertex, destVertex)
                                         }
                                     }
+
                                     else -> {
                                         // no source
                                     }
@@ -83,6 +153,7 @@ object Solver {
             .build().asDot(graph)
         println("----------------------------")
         println(dot)
-        return model
+
+        return Graphs.rootsOf(graph)
     }
 }
