5806345d39c21ba9c3940c3baa47f56bade1a576
Michael Mosmann <michael@mosmann.de>
2024-02-27 11:46:29 +0100

    table and single values

--- src/test/kotlin/de/flapdoodle/tab/app/model/graph/SolverTest.kt
+++ src/test/kotlin/de/flapdoodle/tab/app/model/graph/SolverTest.kt
@@ -55,15 +55,15 @@ class SolverTest {
             .add(1,2)
             .add(3,10)
 
-        val constants = Node.Table<Int>(
+        val table = Node.Table<Int>(
             "table", Columns(listOf(x))
         )
         val destination = ColumnId(Int::class)
         val formula = Node.Calculated("calc", Calculations(
             listOf(Calculation.Tabular("y", EvalAdapter("x+2"), destination))
-        ).let { c -> c.connect(c.inputs[0].id, Source.ColumnSource(constants.id, x.id)) })
+        ).let { c -> c.connect(c.inputs[0].id, Source.ColumnSource(table.id, x.id)) })
 
-        val source = Tab2Model(listOf(constants, formula))
+        val source = Tab2Model(listOf(table, formula))
         val changed = Solver.solve(source)
 
         val data = changed.node(formula.id).column(destination)
@@ -72,6 +72,52 @@ class SolverTest {
                 0 to BigDecimal.valueOf(3),
                 1 to BigDecimal.valueOf(4),
                 3 to BigDecimal.valueOf(12)
+            ))
+
+    }
+
+    @Test
+    fun tablesAndValues() {
+        val a = Column("a", Int::class, Int::class)
+            .add(0, 1)
+            .add(1,2)
+            .add(3,10)
+
+        val b = Column("b", Int::class, Int::class)
+            .add(0, 5)
+            .add(2,10)
+            .add(3,5)
+
+        val x = SingleValue("x", Int::class, 1)
+
+        val constants = Node.Constants(
+            "const", SingleValues(
+                listOf(x)
+            )
+        )
+
+        val table = Node.Table<Int>(
+            "table", Columns(listOf(a, b))
+        )
+        val destination = ColumnId(Int::class)
+        val formula = Node.Calculated("calc", Calculations(
+            listOf(Calculation.Tabular("y", EvalAdapter("x+b+c"), destination))
+        ).let { c ->
+            c.connect(c.inputs[0].id, Source.ColumnSource(table.id, a.id))
+                .connect(c.inputs[1].id, Source.ColumnSource(table.id, b.id))
+                .connect(c.inputs[2].id, Source.ValueSource(constants.id, x.id))
+        })
+
+        val source = Tab2Model(listOf(constants, table, formula))
+        val changed = Solver.solve(source)
+
+        val data = changed.node(formula.id).column(destination)
+        assertThat(data.values)
+            .isEqualTo(mapOf(
+                0 to BigDecimal.valueOf(7),
+                1 to BigDecimal.valueOf(8),
+                2 to BigDecimal.valueOf(13),
+                3 to BigDecimal.valueOf(16)
             ))
 
     }
