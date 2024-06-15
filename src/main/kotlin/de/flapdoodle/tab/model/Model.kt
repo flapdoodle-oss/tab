d088cc9cd2ed780aacfe546eeb329e1e4bc6d4f8
Michael Mosmann <michael@mosmann.de>
2024-06-15 10:50:10 +0200

    replace model refactoring ahead

--- src/main/kotlin/de/flapdoodle/tab/model/Model.kt
+++ src/main/kotlin/de/flapdoodle/tab/model/Model.kt
@@ -1,5 +1,6 @@
 package de.flapdoodle.tab.model
 
+import de.flapdoodle.tab.model.change.ModelChange
 import de.flapdoodle.tab.model.modifier.ModifierFactory
 import java.nio.file.Path
 
@@ -13,6 +14,11 @@ data class Model(
         val modifier = ModifierFactory.changes(nodes, change)
         val result: List<Node> = modifier.fold(nodes) { list, m -> m.modify(list) }
         return copy(nodes = result)
+    }
+
+    @Deprecated("use Change")
+    fun apply(change: ModelChange): Model {
+        return copy(nodes = nodes.map { it.apply(change) })
     }
 
     fun nodes() = nodes
