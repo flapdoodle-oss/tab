1e31e32a5d75406a0e0ffc79cec329595c61ee3f
Michael Mosmann <michael@mosmann.de>
2024-06-15 18:38:32 +0200

    works

--- src/main/kotlin/de/flapdoodle/tab/model/change/ModelChange2ChangeTranslator.kt
+++ src/main/kotlin/de/flapdoodle/tab/model/change/ModelChange2ChangeTranslator.kt
@@ -4,6 +4,10 @@ import de.flapdoodle.tab.model.changes.Change
 
 object ModelChange2ChangeTranslator {
     fun asChange(modelChange: ModelChange): Change? {
-        return null
+        return when (modelChange) {
+            is ModelChange.AddColumn<out Comparable<*>> -> Change.Table.AddColumn(modelChange.id, modelChange.column)
+            is ModelChange.RemoveColumn -> Change.Table.RemoveColumn(modelChange.id, modelChange.columnId)
+            else -> null
+        }
     }
 }
