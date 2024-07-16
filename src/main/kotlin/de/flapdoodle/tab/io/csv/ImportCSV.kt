f805aa0c8dad78e0af76e0a0f27448eccd5d83ab
Michael Mosmann <michael@mosmann.de>
2024-07-16 20:42:35 +0200

    import stuff

--- src/main/kotlin/de/flapdoodle/tab/io/csv/ImportCSV.kt
+++ src/main/kotlin/de/flapdoodle/tab/io/csv/ImportCSV.kt
@@ -2,6 +2,10 @@ package de.flapdoodle.tab.io.csv
 
 import com.opencsv.CSVParserBuilder
 import com.opencsv.CSVReaderBuilder
+import de.flapdoodle.reflection.TypeInfo
+import de.flapdoodle.tab.model.Node
+import de.flapdoodle.tab.model.Title
+import de.flapdoodle.tab.ui.views.calculations.Columns
 import java.io.Reader
 import java.util.*
 
@@ -39,6 +43,8 @@ object ImportCSV {
                     .build()
             )
             .build().use { csvReader ->
+                columnsOf(config)
+
                 val rows = csvReader.readAll()
                 rows.forEachIndexed { index, row ->
                     if (index < config.headerRows) {
@@ -56,6 +62,34 @@ object ImportCSV {
                     }
                 }
             }
+    }
+
+    private fun columnsOf(config: ColumnConfig) {
+        val indexType = indexTypeOf(config)
+        if (indexType != null) {
+            columnsOf(config, indexType)
+        }
+    }
+
+    private fun columnsOf(config: ColumnConfig, indexType: TypeInfo<out Comparable<*>>) {
+        // TODO HACK
+//        val table = tableOf(indexType)
+    }
+
+    private fun <K: Comparable<K>> tableOf(indexType: TypeInfo<K>): Node.Table<K> {
+        return Node.Table(
+            name = Title("foo"),
+            indexType = indexType
+        )
+    }
+
+    private fun indexTypeOf(config: ColumnConfig): TypeInfo<out Comparable<*>>? {
+        val indexConverter = requireNotNull(config.converter[config.indexColumn]) { "no converter for index ${config.indexColumn}" }
+        val indexType = indexConverter.type
+        if (TypeInfo.of(Comparable::class.java).isAssignable(indexType)) {
+            return indexType as TypeInfo<Comparable<*>>
+        }
+        return null
     }
 
 }
